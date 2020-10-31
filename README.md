dae81a42b97c799da1636b20b584878ff6c3d604
Omar Baeraqi <omarbaeraqi@yahoo.com>
2020-10-31 18:39:09 +0300

    Add files via upload
    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Landing Page Project
+Udacity's second project in front end web development nanodegree.
+## Table of Contents
+- index.html 
+Starter code provided by the course.
+- styles.css
+Styling for the provided HTML.
+- app.js
+JavaScript file containing all app's functionality.
+* [Instructions](#instructions)
+
+## Instructions
+
+The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+
+To get started, open `js/app.js` and start building out the app's functionality
+
+For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
---
+++ js/app.js
@@ -0,0 +1,87 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+let sections = document.querySelectorAll("section");
+let navbar = document.getElementById("navbar__list");
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+function buildmenu(){
+    for (i=0; i<sections.length; i++){
+        let item = document.createElement("li");
+        item.className = "menu__link";
+        item.setAttribute("data-nav", sections[i].id);
+        item.textContent = sections[i].getAttribute("data-nav");
+        navbar.appendChild(item);
+    };
+}
+
+// Add class 'active' to section when near top of viewport
+function setsectionactive(){
+    const options = {
+        threshold: 0.8
+    };
+    const observer = new IntersectionObserver(function(containers, observer){
+        containers.forEach(container => {
+            if (!container.isIntersecting){
+                container.target.classList.remove("active");
+                return;
+            }
+            container.target.classList.add("active");
+        });
+    }, options);
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+}
+
+// Scroll to anchor ID using scrollTO event
+function scroll(){
+    navbar.addEventListener("click", function(evt){
+        let anchor = document.querySelector("#" + evt.target.getAttribute("data-nav"));
+        anchor.scrollIntoView({behavior: "smooth"});
+    });
+};
+
+/**
+ * End Main Functions
+ * Begin Events
+ * 
+*/
+
+// Build menu 
+buildmenu();
+// Scroll to section on link click
+scroll();
+// Set sections as active
+setsectionactive();
+
